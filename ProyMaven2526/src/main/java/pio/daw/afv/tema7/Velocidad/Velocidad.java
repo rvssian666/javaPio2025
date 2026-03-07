c6eb3ab9f477277fb150fcf858b51b89c652712c
Alexander <rvssian66@hotmail.com>
2026-03-07 16:46:40 +0100

    Ejercicio calculo velocidad(first)

---
+++ ProyMaven2526/src/main/java/pio/daw/afv/tema7/Velocidad/Velocidad.java
@@ -0,0 +1,53 @@
+package pio.daw.afv.tema7.Velocidad;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Velocidad {
+
+	public static void main(String[] args) {
+
+		Scanner sc = null;
+
+		try {
+			File veloc = new File("C:\\Users\\rvssi\\git\\javaPio2025\\ProyMaven2526\\src\\main\\java\\pio\\daw\\afv\\tema7\\Velocidad\\velocidad.txt");
+			sc = new Scanner(veloc);
+			System.out.println("*Inicio*");
+			// inicializamos casos de pruebas
+			int numCasos = sc.nextInt();
+			for (int i = 0; i < numCasos; i++) {
+				// declaramos variables para guardar
+				// tiempo=t, distancia =d, velocidad =v,
+				double d = 0;
+				double t = 0;
+				double v = 0;
+				for (int j = 0; j < 2; j++) {
+					String line = sc.nextLine();
+					String[] data = line.split("=");
+					if (data[0].equals("D"))
+						d = Double.parseDouble(data[1]);
+					else if (data[0].equals("T"))
+						t = Double.parseDouble(data[1]);
+					else if (data[0].equals("V"))
+						v = Double.parseDouble(data[1]);
+				}
+				if (v == 0)
+					System.out.println("Velocidad= " + d / t);
+				else if (t == 0)
+					System.out.println("Tiempo= " + d / v);
+				else
+					System.out.println("Distancia= " + (v * t));
+
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Error " + e.getMessage());
+		} finally {
+			if (sc != null)
+				sc.close();
+		}
+
+	}
+
+}
