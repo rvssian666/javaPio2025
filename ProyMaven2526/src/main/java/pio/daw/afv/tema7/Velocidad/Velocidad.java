54debe14679d9751b66d89db6e8ca6d9ed8c25e7
Alexander <rvssian66@hotmail.com>
2026-03-08 11:58:21 +0100

    Complete

--- ProyMaven2526/src/main/java/pio/daw/afv/tema7/Velocidad/Velocidad.java
+++ ProyMaven2526/src/main/java/pio/daw/afv/tema7/Velocidad/Velocidad.java
@@ -11,20 +11,26 @@ public class Velocidad {
 		Scanner sc = null;
 
 		try {
-			File veloc = new File("C:\\Users\\rvssi\\git\\javaPio2025\\ProyMaven2526\\src\\main\\java\\pio\\daw\\afv\\tema7\\Velocidad\\velocidad.txt");
+			File veloc = new File(
+					"C:\\Users\\rvssi\\git\\javaPio2025\\ProyMaven2526\\src\\main\\java\\pio\\daw\\afv\\tema7\\Velocidad\\velocidad.txt");
 			sc = new Scanner(veloc);
 			System.out.println("*Inicio*");
 			// inicializamos casos de pruebas
 			int numCasos = sc.nextInt();
+			System.out.println("numCasos= " + 3);
+			sc.nextLine();
 			for (int i = 0; i < numCasos; i++) {
 				// declaramos variables para guardar
 				// tiempo=t, distancia =d, velocidad =v,
 				double d = 0;
 				double t = 0;
 				double v = 0;
+
+				String line = sc.nextLine();
+				System.out.println(line);
+				String[] espacios = line.split(" ");
 				for (int j = 0; j < 2; j++) {
-					String line = sc.nextLine();
-					String[] data = line.split("=");
+					String[] data = espacios[j].split("=");
 					if (data[0].equals("D"))
 						d = Double.parseDouble(data[1]);
 					else if (data[0].equals("T"))
